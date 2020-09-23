bfb6def6619083ac7d8c1b36651aceef9f588a16
David Liongson <david@liongson.co>
2020-09-23 21:08:00 +0800

    Day 36 - Multiplication app

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by David Liongson on 9/10/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
