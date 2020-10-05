9be92fc3da84e3aa9217629f8eea33242dd7dc3a
David Liongson <david@liongson.co>
2020-10-05 22:00:37 +0800

    Day 43

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by David Liongson on 10/5/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
