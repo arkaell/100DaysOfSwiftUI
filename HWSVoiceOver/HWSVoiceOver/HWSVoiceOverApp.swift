658268f21d99fe5c6ad21057d930da7f22efebd7
David Liongson <david@houseofdavid.dev>
2021-03-09 18:05:42 +0800

    Day 76

---
+++ HWSVoiceOver/HWSVoiceOver/HWSVoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSVoiceOverApp.swift
+//  HWSVoiceOver
+//
+//  Created by David Liongson on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct HWSVoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
